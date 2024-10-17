5f80e204e192ac8d4429368c7378a727384492ef
Nicolas Roger <nramonteirods05@gmail.com>
2024-10-17 17:56:53 +0000

    criando_menu_e_funcao_para_limpar_terminal

---
+++ arquivo_nicolas.py
@@ -0,0 +1,14 @@
+from os import system
+
+print("""   === RH System ===
+1 - Adicionar um funcionário
+2 - Consultar um funcionário
+3 - Atualizar os dados de um funcionário
+4 - Excluir um funcionário
+5 - Listar todos os funcionários
+0 - Sair do sistema.
+""")
+# O menu deverá ser exibido após utilizar uma das suas opções
+
+def limpar_terminal():
+    system("cls || clear")
